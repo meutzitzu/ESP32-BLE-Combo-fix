f9a57a0b0bc5bd4a5cb2b1803bd8a08aa9c35635
Peter Pakanun <lpeachl@gmail.com>
2022-01-01 15:40:31 +0700

    change report id of the mouse

--- BleCombo.cpp
+++ BleCombo.cpp
@@ -28,7 +28,7 @@
 // Report IDs:
 #define KEYBOARD_ID 0x01
 #define MEDIA_KEYS_ID 0x02
-#define MOUSE 0x03
+#define MOUSE_ID 0x03
 
 static const uint8_t _hidReportDescriptor[] = {
   USAGE_PAGE(1),      0x01,          // USAGE_PAGE (Generic Desktop Ctrls)
@@ -94,44 +94,45 @@ static const uint8_t _hidReportDescriptor[] = {
   HIDINPUT(1),        0x02,          //   INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
   END_COLLECTION(0),                 // END_COLLECTION
   // ------------------------------------------------- Mouse
-  USAGE_PAGE(1),       0x01, // USAGE_PAGE (Generic Desktop)
-  USAGE(1),            0x02, // USAGE (Mouse)
-  COLLECTION(1),       0x01, // COLLECTION (Application)
-  USAGE(1),            0x01, //   USAGE (Pointer)
-  COLLECTION(1),       0x00, //   COLLECTION (Physical)
+  USAGE_PAGE(1),       0x01,         // USAGE_PAGE (Generic Desktop)
+  USAGE(1),            0x02,         // USAGE (Mouse)
+  COLLECTION(1),       0x01,         // COLLECTION (Application)
+  USAGE(1),            0x01,         //   USAGE (Pointer)
+  COLLECTION(1),       0x00,         //   COLLECTION (Physical)
+	REPORT_ID(1),        MOUSE_ID,     //     REPORT_ID (1)
   // ------------------------------------------------- Buttons (Left, Right, Middle, Back, Forward)
-  USAGE_PAGE(1),       0x09, //     USAGE_PAGE (Button)
-  USAGE_MINIMUM(1),    0x01, //     USAGE_MINIMUM (Button 1)
-  USAGE_MAXIMUM(1),    0x05, //     USAGE_MAXIMUM (Button 5)
-  LOGICAL_MINIMUM(1),  0x00, //     LOGICAL_MINIMUM (0)
-  LOGICAL_MAXIMUM(1),  0x01, //     LOGICAL_MAXIMUM (1)
-  REPORT_SIZE(1),      0x01, //     REPORT_SIZE (1)
-  REPORT_COUNT(1),     0x05, //     REPORT_COUNT (5)
-  HIDINPUT(1),         0x02, //     INPUT (Data, Variable, Absolute) ;5 button bits
+  USAGE_PAGE(1),       0x09,         //     USAGE_PAGE (Button)
+  USAGE_MINIMUM(1),    0x01,         //     USAGE_MINIMUM (Button 1)
+  USAGE_MAXIMUM(1),    0x05,         //     USAGE_MAXIMUM (Button 5)
+  LOGICAL_MINIMUM(1),  0x00,         //     LOGICAL_MINIMUM (0)
+  LOGICAL_MAXIMUM(1),  0x01,         //     LOGICAL_MAXIMUM (1)
+  REPORT_SIZE(1),      0x01,         //     REPORT_SIZE (1)
+  REPORT_COUNT(1),     0x05,         //     REPORT_COUNT (5)
+  HIDINPUT(1),         0x02,         //     INPUT (Data, Variable, Absolute) ;5 button bits
   // ------------------------------------------------- Padding
-  REPORT_SIZE(1),      0x03, //     REPORT_SIZE (3)
-  REPORT_COUNT(1),     0x01, //     REPORT_COUNT (1)
-  HIDINPUT(1),         0x03, //     INPUT (Constant, Variable, Absolute) ;3 bit padding
+  REPORT_SIZE(1),      0x03,         //     REPORT_SIZE (3)
+  REPORT_COUNT(1),     0x01,         //     REPORT_COUNT (1)
+  HIDINPUT(1),         0x03,         //     INPUT (Constant, Variable, Absolute) ;3 bit padding
   // ------------------------------------------------- X/Y position, Wheel
-  USAGE_PAGE(1),       0x01, //     USAGE_PAGE (Generic Desktop)
-  USAGE(1),            0x30, //     USAGE (X)
-  USAGE(1),            0x31, //     USAGE (Y)
-  USAGE(1),            0x38, //     USAGE (Wheel)
-  LOGICAL_MINIMUM(1),  0x81, //     LOGICAL_MINIMUM (-127)
-  LOGICAL_MAXIMUM(1),  0x7f, //     LOGICAL_MAXIMUM (127)
-  REPORT_SIZE(1),      0x08, //     REPORT_SIZE (8)
-  REPORT_COUNT(1),     0x03, //     REPORT_COUNT (3)
-  HIDINPUT(1),         0x06, //     INPUT (Data, Variable, Relative) ;3 bytes (X,Y,Wheel)
+  USAGE_PAGE(1),       0x01,         //     USAGE_PAGE (Generic Desktop)
+  USAGE(1),            0x30,         //     USAGE (X)
+  USAGE(1),            0x31,         //     USAGE (Y)
+  USAGE(1),            0x38,         //     USAGE (Wheel)
+  LOGICAL_MINIMUM(1),  0x81,         //     LOGICAL_MINIMUM (-127)
+  LOGICAL_MAXIMUM(1),  0x7f,         //     LOGICAL_MAXIMUM (127)
+  REPORT_SIZE(1),      0x08,         //     REPORT_SIZE (8)
+  REPORT_COUNT(1),     0x03,         //     REPORT_COUNT (3)
+  HIDINPUT(1),         0x06,         //     INPUT (Data, Variable, Relative) ;3 bytes (X,Y,Wheel)
   // ------------------------------------------------- Horizontal wheel
-  USAGE_PAGE(1),       0x0c, //     USAGE PAGE (Consumer Devices)
-  USAGE(2),      0x38, 0x02, //     USAGE (AC Pan)
-  LOGICAL_MINIMUM(1),  0x81, //     LOGICAL_MINIMUM (-127)
-  LOGICAL_MAXIMUM(1),  0x7f, //     LOGICAL_MAXIMUM (127)
-  REPORT_SIZE(1),      0x08, //     REPORT_SIZE (8)
-  REPORT_COUNT(1),     0x01, //     REPORT_COUNT (1)
-  HIDINPUT(1),         0x06, //     INPUT (Data, Var, Rel)
-  END_COLLECTION(0),         //   END_COLLECTION
-  END_COLLECTION(0)          // END_COLLECTION
+  USAGE_PAGE(1),       0x0c,         //     USAGE PAGE (Consumer Devices)
+  USAGE(2),      0x38, 0x02,         //     USAGE (AC Pan)
+  LOGICAL_MINIMUM(1),  0x81,         //     LOGICAL_MINIMUM (-127)
+  LOGICAL_MAXIMUM(1),  0x7f,         //     LOGICAL_MAXIMUM (127)
+  REPORT_SIZE(1),      0x08,         //     REPORT_SIZE (8)
+  REPORT_COUNT(1),     0x01,         //     REPORT_COUNT (1)
+  HIDINPUT(1),         0x06,         //     INPUT (Data, Var, Rel)
+  END_COLLECTION(0),                 //   END_COLLECTION
+  END_COLLECTION(0)                  // END_COLLECTION
 };
 
 BleCombo::BleCombo(std::string deviceName, std::string deviceManufacturer, uint8_t batteryLevel)
@@ -151,7 +152,7 @@ void BleCombo::begin(void)
   inputKeyboard = hid->inputReport(KEYBOARD_ID);  // <-- input REPORTID from report map
   outputKeyboard = hid->outputReport(KEYBOARD_ID);
   inputMediaKeys = hid->inputReport(MEDIA_KEYS_ID);
-  inputMouse = hid->inputReport(MOUSE);
+  inputMouse = hid->inputReport(MOUSE_ID);
 
   outputKeyboard->setCallbacks(this);
 
